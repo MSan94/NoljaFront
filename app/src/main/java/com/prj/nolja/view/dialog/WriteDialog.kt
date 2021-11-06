648df162b0e601888d48e057fb103258a0543e42
MyeongSeong <ms1994s@naver.com>
2021-11-07 05:04:44 +0900

    이미지 파일 미리보기

--- app/src/main/java/com/prj/nolja/view/dialog/WriteDialog.kt
+++ app/src/main/java/com/prj/nolja/view/dialog/WriteDialog.kt
@@ -1,21 +1,32 @@
 package com.prj.nolja.view.dialog
 
+import android.R.attr
+import android.app.Activity.RESULT_OK
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Point
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.text.TextUtils
 import android.util.Log
 import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import com.prj.nolja.MyApplication
-import com.prj.nolja.MyApplication.Companion.size
 import com.prj.nolja.databinding.DialogWriteBinding
+
 
 class WriteDialog : DialogFragment() {
     private var _binding : DialogWriteBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = DialogWriteBinding.inflate(inflater,container,false)
+        binding.btnFile.setOnClickListener {
+            onPermission()
+        }
         return binding.root
     }
 
@@ -46,5 +57,38 @@ class WriteDialog : DialogFragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    fun onPermission(){
+        var cameraPermission = context?.let { ContextCompat.checkSelfPermission(it, android.Manifest.permission.CAMERA) }
+        if(cameraPermission == PackageManager.PERMISSION_DENIED){
+            requestPermission()
+        }else{
+            selectCamera()
+        }
+    }
+    fun requestPermission(){
+        ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA), 99)
+    }
+
+    fun selectCamera(){
+        val SELECT_PICTURE : Int = 200;
+        var state = Environment.getExternalStorageState()
+        if(TextUtils.equals(state,Environment.MEDIA_MOUNTED)){
+            var intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(Intent.createChooser(intent,"Select"),SELECT_PICTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            if(requestCode == 200){
+                var selectImg : Uri = data!!.data
+                binding.imageViewImg.setImageURI(selectImg)
+
+            }
+        }
     }
 }
